0ef1d8780885cbcbae72c6b366f957107129a440
Cristian Poll <me@cristianpoll.com>
2023-05-07 23:46:06 -0400

    add jumping

--- godotproject/humanoid.gd
+++ godotproject/humanoid.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 @onready var animation_tree = $AnimationTree
 
 const MAX_SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 5.5
 const LERP_VAL = 0.15
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -31,9 +31,21 @@ func _physics_process(delta):
     if not is_on_floor():
         velocity.y -= gravity * delta
 
-#    # Handle Jump.
-#    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#        velocity.y = JUMP_VELOCITY
+    # Handle Jump.
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
+        
+        # TODO: Unjank animation
+        $AnimationTree.set("parameters/jump/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+    # Set falling animation
+    if not is_on_floor() and not (animation_tree.get("parameters/fall/active") or animation_tree.get("parameters/jump/active")):
+        $AnimationTree.set("parameters/fall/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+    # Unset jumping and falling when on the ground
+    if is_on_floor() and (animation_tree.get("parameters/jump/active") or animation_tree.get("parameters/fall/active")):
+        $AnimationTree.set("parameters/jump/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+        $AnimationTree.set("parameters/fall/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
 
     # Get the input direction and handle the movement/deceleration.
     var input_dir = Input.get_vector("left", "right", "forward", "back")
@@ -52,6 +64,6 @@ func _physics_process(delta):
         velocity.z = lerp(direction.z, 0.0, LERP_VAL)
 
     print(velocity.length() / MAX_SPEED)
-    animation_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / MAX_SPEED)
+    animation_tree.set("parameters/walkrun/blend_position", velocity.length() / MAX_SPEED)
 
     move_and_slide()
